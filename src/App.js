ea259979526e8abe1f8ef949745b52523550ee3a
Gustavo Rosa <gustavorib.rosa@gmail.com>
2024-05-16 15:07:53 -0300

    example without a custom hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setIsVisible((prev) => !prev)}>
+        {isVisible ? "Hide" : "Show"}
+      </button>
+      {isVisible && <h1> Hidden text </h1>}
     </div>
   );
 }
